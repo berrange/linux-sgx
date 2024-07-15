2b4ddd36300ae147e564fe216fb41c69c2eb2cbb
Daniel P. Berrangé <berrange@redhat.com>
2024-07-15 17:17:27 +0100

    Set dynamic linker interpretor path
    The ld-linux interpretor is unused in SGX context, but a build system
    mistake means it is still present. Thus to ensure reproducible builds
    we must set the NixOS interpretor path.
    
    Signed-off-by: Daniel P. Berrangé <berrange@redhat.com>

--- psw/ae/buildenv.mk
+++ psw/ae/buildenv.mk
@@ -76,7 +76,8 @@ CFLAGS    += $(ENCLAVE_CFLAGS)
 
 LDTFLAGS  = -L$(SGX_LIB_DIR) -Wl,--whole-archive $(TRTSLIB) -Wl,--no-whole-archive \
             -Wl,--start-group $(EXTERNAL_LIB) -Wl,--end-group -Wl,--build-id       \
-            -Wl,--version-script=$(ROOT_DIR)/build-scripts/enclave.lds $(ENCLAVE_LDFLAGS)
+            -Wl,--version-script=$(ROOT_DIR)/build-scripts/enclave.lds $(ENCLAVE_LDFLAGS) \
+            -Wl,-dynamic-linker,/nix/store/xmprbk52mlcdsljz66m8yf7cf0xf36n1-glibc-2.38-44/lib/ld-linux-x86-64.so.2
 
 LDTFLAGS += -Wl,-Map=out.map -Wl,--undefined=version -Wl,--gc-sections
 
