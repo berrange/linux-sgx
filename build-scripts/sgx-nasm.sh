258dfa3df79b8286549856003e8234975434fbb3
Daniel P. Berrangé <berrange@redhat.com>
2025-04-02 17:11:25 +0100

    Add wrapper for nasm to fix cmake compat
    cmake needs to detect nasm by running with the '-v' arg, but it
    cannot cope with the nasm command being anything other than a
    single binary name - it won't accept & pass on args during the
    detection phase. Thus a further wrapper is needed.
    
    Signed-off-by: Daniel P. Berrangé <berrange@redhat.com>

---
+++ build-scripts/sgx-nasm.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+set -e
+
+if test "$1" == "-v"
+then
+    exec nasm -v
+else
+    here=$(dirname $0)
+    echo python3 ${here}/sgx-asm-pp.py --assembler=nasm --MITIGATION-CVE-2020-0551=${MITIGATION} "$@"
+    exec python3 ${here}/sgx-asm-pp.py --assembler=nasm --MITIGATION-CVE-2020-0551=${MITIGATION} "$@"
+fi
